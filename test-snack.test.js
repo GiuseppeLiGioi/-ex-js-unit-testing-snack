1918b84fc76661f8a41cce185fe53ad1297147ed
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-09 16:42:25 +0200

    added. snack 7

--- test-snack.test.js
+++ test-snack.test.js
@@ -1,13 +1,5 @@
 /*
-🏆 Snack 7
-Crea un array di oggetti posts, in cui ogni oggetto ha le proprietà id, title e slug.
 
-Creare un test che verifichi le seguenti descrizioni:
-
-👉 "La funzione findPostById restituisce il post corretto dato l’array di post e l’id"
-
-
-Creare uno o più test aggiuntivi che controllino che la struttura dati passati sia conforme (ogni post ha le proprietà id, title e slug, viene passato un id numerico).
 */
     
     
@@ -56,4 +48,36 @@ test('La funzione "isPalindrome" verifica se una stringa è un palindromo.', () 
 test('La funzione "createSlugTitle" lancia un errore se il titolo è vuoto o non valido.', () => {
     expect(() => createSlugTitle("")).toThrow()
     expect(() => createSlugTitle("ciao@ciao")).toThrow()
+})
+
+const posts = [
+  {
+    id: 1,
+    title: "Ciao Mondo",
+    slug: "ciao-mondo"
+  },
+  {
+    id: 2,
+    title: "Introduzione a JavaScript",
+    slug: "introduzione-a-javascript"
+  },
+  {
+    id: 3,
+    title: "Come creare uno slug",
+    slug: "come-creare-uno-slug"
+  }
+];
+
+function findPostById(arrPosts, id){
+    if(isNaN(id)){
+        throw new Error("l'id deve essere un numero intero")
+    }
+    const findPost = arrPosts.find((p) => p.id === parseInt(id));
+    return findPost
+}
+
+test('La funzione "findPostById" restituisce il post corretto dato l’array di post e l’id', () => {
+    expect(findPostById(posts, 2)).toEqual({id: 2,title: "Introduzione a JavaScript",slug: "introduzione-a-javascript"})
+    expect(() => findPostById(posts, "ciao")).toThrow()
+    
 })
